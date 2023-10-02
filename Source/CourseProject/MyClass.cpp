52ba993aa847aa402ad89cb13a062f4d617cfcdb
MEgorov4 <47919432+MEgorov4@users.noreply.github.com>
2023-10-02 15:24:36 +0500

    Update lesson 1

---
+++ Source/CourseProject/MyClass.cpp
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyClass.h"
+
+void UMyClass::PostInitProperties()
+{
+	UObject::PostInitProperties();
+
+	TArray<int> Array = {1, 2, 3, 4, 5, 6}; // Init
+	Array.Add(7); // Add element
+	Array.Insert(0, 0); // Insert element
+	Array.RemoveAt(0); // Remove element
+	size_t ArrayNum = Array.Num(); // Get the number of elements
+	Array.SetNum(ArrayNum + 1); // Set the number of elements
+
+	for (int i = 0; i < Array.Num(); i++) // With counter
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Array[%d] = %d"), i, Array[i]);
+	}
+
+	for (auto element : Array) // For each element
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Array[%d] = %d"), element);
+	}
+	
+	for (auto it = Array.begin(); it != Array.end(); ++it) // With iterators
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Array[%d] = %d"), *it);
+	}
+}
+
+class Human
+{
+	short phoneNumber; // скрытые поля
+	int age; // скрытые поля
+	void CalculateNextBusinessDay(); // скрытые методы
+public:
+	Human(short phoneNumber, int age);
+};
---
+++ Source/CourseProject/MyClass.h
@@ -0,0 +1,63 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "MyClass.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COURSEPROJECT_API UMyClass : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void PrintString(const FString& Message);
+
+
+	virtual void PostInitProperties() override;
+
+	//Числовые типы 
+	uint8 Byte;
+	int32 Int;
+	int64 Int64;
+	float Float;
+
+	
+	bool Bool;
+	// Строковые типы
+	FName Name;
+	FText Text;
+	FString String;
+
+	FVector	Vector;
+	FRotator Rotator;
+	FTransform Transform;
+
+	
+	
+	UObject* Object;
+};
+
+USTRUCT(BlueprintType)
+struct FMyVector2D
+{
+	GENERATED_BODY()
+	int X;
+	int Y;
+};
+
+UENUM(BlueprintTYpe)
+enum class EPlayerState : uint8
+{
+		EPS_Idle = 0 UMETA(DisplayName = "Idle"),
+		EPS_Walking = 1 UMETA(DisplayName = "Walking"),
+        EPS_Running = 2 UMETA(DisplayName = "Running"),
+        EPS_Jumping = 3 UMETA(DisplayName = "Jumping"),
+        EPS_Falling = 4 UMETA(DisplayName = "Falling"),
+        EPS_Swimming = 5 UMETA(DisplayName = "Swimming"),
+};
